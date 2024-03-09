cb78e8a06645ad46e1b0abf2f4bb86b3a714fa24
Angelo Barcelos <angelorodrigues318@gmail.com>
2024-03-09 15:07:45 -0300

    Append method

--- Linked_List/src/App.java
+++ Linked_List/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        LinkedList myLinkedList = new LinkedList();
+
+        myLinkedList.append(2);
+
+        myLinkedList.printList();
     }
 }
--- Linked_List/src/LinkedList.java
+++ Linked_List/src/LinkedList.java
@@ -13,10 +13,44 @@ public class LinkedList {
     private Node tail;
     private int length;
 
+    public LinkedList(){}
+
     public LinkedList(int value){
         Node newNode = new Node(value);
         head = newNode;
         tail = newNode;
         length = 1;
+    }
+
+    public void append(int value){
+        Node newNode = new Node(value);
+        if(head == null && tail == null){
+            head = newNode;
+            tail = newNode;
+        }else{
+            tail.next = newNode;
+            tail = newNode;            
+        }
+        length ++;
+    }
+
+    public void printList(){
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getHead() {
+        System.out.println("Head: " + head.value);
+    }
+
+    public void getTail() {
+        System.out.println("Tail: " + tail.value);
+    }
+
+    public void getLength() {
+        System.out.println("Length " + length);
     }
 }
